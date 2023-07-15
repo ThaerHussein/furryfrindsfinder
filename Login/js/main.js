e9d68b38b48c677e2928c4d1844e4330594b144f
Mohammad Al-Haimouni <m.alhaimouni@gmail.com>
2023-07-15 11:18:08 +0300

    main index

--- Login/js/main.js
+++ Login/js/main.js
@@ -3,12 +3,11 @@
 let login = document.querySelector("#log-btn");
 login.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("ali");
   const user = {
     name: "ali",
     age: 25,
     email: "test@test.com",
   };
   localStorage.setItem("user", JSON.stringify(user));
-  window.location.href = 'http://127.0.0.1:5500/home_page/index.html';
+  window.location.href = '../../home_page/index.html';
 });
